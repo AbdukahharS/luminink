279f2305482f9d667885c886f5369972aad367f1
AbdukahharS <shakhzodbekkakhkhorov04@gmail.com>
2024-04-16 10:49:51 +0800

    Configuration and folder structure

---
+++ app/(marketing)/_components/Heading.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+const Heading = () => {
+  return (
+    <div className='max-w-3xl space-y-4'>
+      <h1 className='text-3xl sm:text-5xl md:text-6xl'>
+        Illuminate Your Ideas, Capture Your Creativity. Welcome to{' '}
+        <span className='underline'>LuminInk</span>
+      </h1>
+    </div>
+  )
+}
+
+export default Heading
