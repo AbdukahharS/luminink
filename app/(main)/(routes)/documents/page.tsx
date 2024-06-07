77043a697a40dbf7a9c4a28b4ca4eadf48fefd23
AbdukahharS <shakhzodbekkakhkhorov04@gmail.com>
2024-06-07 23:40:57 +0800

    Sidebar

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,9 @@
+const DocumentsPage = () => {
+  return (
+    <div>
+      <h1>This is a protected Documents page</h1>
+    </div>
+  )
+}
+
+export default DocumentsPage
---
+++ app/(main)/layout.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { useConvexAuth } from 'convex/react'
+import { redirect } from 'next/navigation'
+
+import { Spinner } from '@/components/Spinner'
+
+import Navigation from './_components/Navigation'
+
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  const { isAuthenticated, isLoading } = useConvexAuth()
+
+  if (isLoading) {
+    return (
+      <div className='h-full flex items-center justify-center'>
+        <Spinner size='lg' />
+      </div>
+    )
+  }
+
+  if (!isAuthenticated) {
+    return redirect('/')
+  }
+
+  return (
+    <div className='h-full flex dark:ng-[#1f1f1f]'>
+      <Navigation />
+      <main className='h-full flex-1 overflow-y-auto'>{children}</main>
+    </div>
+  )
+}
+
+export default MainLayout
