493e64b54897d363997fade7eacc7f3aeb947ae1
AbdukahharS <shakhzodbekkakhkhorov04@gmail.com>
2024-06-08 13:37:11 +0800

    Sidebar Item

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -1,20 +1,29 @@
 'use client'
 
+import {
+  ChevronsLeft,
+  MenuIcon,
+  PlusCircle,
+  Search,
+  Settings,
+} from 'lucide-react'
+import { usePathname } from 'next/navigation'
 import { ElementRef, useEffect, useRef, useState } from 'react'
 import { useMediaQuery } from 'usehooks-ts'
-import { useQuery } from 'convex/react'
+import { useMutation, useQuery } from 'convex/react'
+import { toast } from 'sonner'
 
-import { ChevronsLeft, MenuIcon } from 'lucide-react'
-import { usePathname } from 'next/navigation'
 import { cn } from '@/lib/utils'
 import { api } from '@/convex/_generated/api'
 
 import UserItem from './UserItem'
+import Item from './Item'
 
 const Navigation = () => {
   const pathname = usePathname()
   const isMobile = useMediaQuery('(max-width: 768px)')
   const documents = useQuery(api.documents.get)
+  const create = useMutation(api.documents.create)
 
   const isResizingRef = useRef(false)
   const sidebarRef = useRef<ElementRef<'aside'>>(null)
@@ -101,6 +110,16 @@ const Navigation = () => {
     }
   }
 
+  const handleCreate = () => {
+    const promise = create({ title: 'Untitled' })
+
+    toast.promise(promise, {
+      loading: 'Creating a new note...',
+      success: 'New note created!',
+      error: 'Failed to create a new note',
+    })
+  }
+
   return (
     <>
       <aside
@@ -123,6 +142,9 @@ const Navigation = () => {
         </div>
         <div>
           <UserItem />
+          <Item label='Search' icon={Search} isSearch onClick={() => {}} />
+          <Item label='Settings' icon={Settings} onClick={() => {}} />
+          <Item onClick={handleCreate} label='New Page' icon={PlusCircle} />
         </div>
         <div className='mt-4'>
           {documents?.map((document) => (
