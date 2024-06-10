3684a8b9f2819882b19375704fe598fbab3122f6
AbdukahharS <shakhzodbekkakhkhorov04@gmail.com>
2024-06-11 05:19:35 +0800

    Navbar

---
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const DocumentIdPage = () => {
+  return <div>DocumentIdPage</div>
+}
+
+export default DocumentIdPage
--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -9,7 +9,7 @@ import {
   Settings,
   Trash,
 } from 'lucide-react'
-import { usePathname } from 'next/navigation'
+import { useParams, usePathname } from 'next/navigation'
 import { ElementRef, useEffect, useRef, useState } from 'react'
 import { useMediaQuery } from 'usehooks-ts'
 import { useMutation } from 'convex/react'
@@ -29,11 +29,13 @@ import UserItem from './UserItem'
 import Item from './Item'
 import DocumentList from './DocumentList'
 import TrashBox from './TrashBox'
+import Navbar from './Navbar'
 
 const Navigation = () => {
   const settings = useSettings()
   const search = useSearch()
   const pathname = usePathname()
+  const params = useParams()
   const isMobile = useMediaQuery('(max-width: 768px)')
   const create = useMutation(api.documents.create)
 
@@ -187,15 +189,19 @@ const Navigation = () => {
           isMobile && 'left-0 w-full'
         )}
       >
-        <nav className='bg-transparent px-3 py-2 w-full'>
-          {isCollapsed && (
-            <MenuIcon
-              onClick={resetWidth}
-              role='button'
-              className='h-6 w-6 text-muted-foreground'
-            />
-          )}
-        </nav>
+        {!!params.documentId ? (
+          <Navbar isCollapsed={isCollapsed} onResetWidth={resetWidth} />
+        ) : (
+          <nav className='bg-transparent px-3 py-2 w-full'>
+            {isCollapsed && (
+              <MenuIcon
+                onClick={resetWidth}
+                role='button'
+                className='h-6 w-6 text-muted-foreground'
+              />
+            )}
+          </nav>
+        )}
       </div>
     </>
   )
