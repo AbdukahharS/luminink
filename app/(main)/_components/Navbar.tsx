7f75fc2021c9731c06eb4513c16528e570d022a4
AbdukahharS <shakhzodbekkakhkhorov04@gmail.com>
2024-06-12 16:24:26 +0800

    Banner component

--- app/(main)/_components/Navbar.tsx
+++ app/(main)/_components/Navbar.tsx
@@ -8,6 +8,8 @@ import { api } from '@/convex/_generated/api'
 import { Id } from '@/convex/_generated/dataModel'
 
 import Title from './Title'
+import Banner from './Banner'
+import Menu from './Menu'
 
 interface NavbarProps {
   isCollapsed: boolean
@@ -23,8 +25,11 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
 
   if (document === undefined) {
     return (
-      <nav className='bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center'>
+      <nav className='bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center justify-between'>
         <Title.Skeleton />
+        <div className='flex items-center gap-x-2'>
+          <Menu.Skeleton />
+        </div>
       </nav>
     )
   }
@@ -45,8 +50,12 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
         )}
         <div className='flex items-center justify-between w-full'>
           <Title initialData={document} />
+          <div className='flex items-center gap-x-2'>
+            <Menu documentId={document._id} />
+          </div>
         </div>
       </nav>
+      {document.isArchived && <Banner documentId={document._id} />}
     </>
   )
 }
